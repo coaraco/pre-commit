a3119a4e06ddb1cb753ff61b6c2597d7f2187bc7
Miquel Ferrer Llompart <hola@miquelferrerllompart.com>
2020-01-03 12:35:00 +0100

    test

--- src/hooks/git-add.ts
+++ src/hooks/git-add.ts
@@ -3,8 +3,9 @@ import { exec } from "child_process";
 
 export async function gitAdd(): Promise<IResponse> {
   return new Promise(resolve => {
-    exec("git update-index --again", error => {
-      if (error) return resolve({ ok: false, error: error.name });
+    exec("git add $(git diff --name-only --cached)", error => {
+      if (error) return resolve({ ok: false, error: error.name || error.message });
+
       resolve({ ok: true });
     });
   });
