0bd00b9a1d86bab4fc83f443b5c6af5fb4d071e5
Miquel Ferrer Llompart <hola@miquelferrerllompart.com>
2020-01-03 12:22:15 +0100

    test

--- src/hooks/git-add.ts
+++ src/hooks/git-add.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 
 export async function gitAdd(): Promise<IResponse> {
   return new Promise(resolve => {
-    exec("git add -u", error => {
+    exec("git update-index --again", error => {
       if (error) return resolve({ ok: false, error: error.name });
       resolve({ ok: true });
     });
